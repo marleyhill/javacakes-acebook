554a90cb3aba03591dca981b64478c886f93c39b
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-02-13 14:50:13 +0000

    updating heroku pom.xml

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,31 +1,31 @@
-import org.sql2o.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import static jdk.internal.org.jline.utils.Log.error;
-
-public class DB {
-    private static URI dbUri;
-    public static Sql2o sql2o;
-    Logger logger = LoggerFactory.getLogger(DB.class);
-    static {
-
-        try {
-            if (System.getenv("HEROKU_POSTGRESQL_OLIVE_URL") == null) {
-                dbUri = new URI("postgres://localhost:5432/acebook");
-            } else {
-                dbUri = new URI(System.getenv("HEROKU_POSTGRESQL_OLIVE_URL"));
-            }
-            int port = dbUri.getPort();
-            String host = dbUri.getHost();
-            String path = dbUri.getPath();
-            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
-            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
-            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
-        } catch (URISyntaxException e ) {
-            error("Unable to connect to database.");
-        }
-    }
-}
+//import org.sql2o.*;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import static jdk.internal.org.jline.utils.Log.error;
+//
+//public class DB {
+//    private static URI dbUri;
+//    public static Sql2o sql2o;
+//    Logger logger = LoggerFactory.getLogger(DB.class);
+//    static {
+//
+//        try {
+//            if (System.getenv("HEROKU_POSTGRESQL_OLIVE_URL") == null) {
+//                dbUri = new URI("postgres://localhost:5432/acebook");
+//            } else {
+//                dbUri = new URI(System.getenv("HEROKU_POSTGRESQL_OLIVE_URL"));
+//            }
+//            int port = dbUri.getPort();
+//            String host = dbUri.getHost();
+//            String path = dbUri.getPath();
+//            String username = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[0];
+//            String password = (dbUri.getUserInfo() == null) ? null : dbUri.getUserInfo().split(":")[1];
+//            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+//        } catch (URISyntaxException e ) {
+//            error("Unable to connect to database.");
+//        }
+//    }
+//}
